ab405722467891ca04ec4c4ff253e2e96b581450
faruktinaz <faruktinazz@icloud.com>
2024-07-03 02:28:48 +0300

    added core App

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from item.models import Category, Item
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    
+    return render(request, 'core/index.html', {
+		'categories': categories,
+		'items': items,
+	})
+
+def contact(request):
+    return render(request, 'core/contact.html')
