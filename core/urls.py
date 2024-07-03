5478e129703ccb7f4e824b635edf44aaee0f69ac
faruktinaz <faruktinazz@icloud.com>
2024-07-03 17:19:00 +0300

    added signup form, views

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.Signup, name='signup'),
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item
+from .forms import SignUpForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -12,3 +13,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {
+		'form': form
+	})
