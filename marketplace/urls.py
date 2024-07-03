e47fd0797a7e86bd7cee84726dcc6b8e5f21d8b5
faruktinaz <faruktinazz@icloud.com>
2024-07-03 17:20:02 +0300

    db update signup urls.py updated

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -21,8 +21,7 @@ from django.urls import include, path
 from core.views import contact, index
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
