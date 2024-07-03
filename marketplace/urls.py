dba2606a6e50c0a76dae8ce436a94fd9281d0c8b
faruktinaz <faruktinazz@icloud.com>
2024-07-03 03:07:40 +0300

    updated db and urls for item detail page

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from core.views import contact, index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
